98ec7d308e0d3a076da383d53b01d9d66129e4e4
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2023-10-22 18:00:04 +0700

    (Add) Catalogue basic layout

---
+++ resources/views/components/product-card.blade.php
@@ -0,0 +1,3 @@
+<div class="w-[200px] aspect-[3/4] bg-[#EAE5D5] border border-black">
+    This is the product card
+</div>
--- resources/views/screens/catalogue.blade.php
+++ resources/views/screens/catalogue.blade.php
@@ -1,7 +1,70 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="p-6">
-        <h1>Catalogue Page</h1>
+<div class="flex flex-col">
+
+    <!-- Hero Section -->
+    <div class="hero bg-hero min-h-[50vh]">
+        <h1 class="text-6xl text-white">Catalogue Page</h1>
     </div>
+
+    <!-- Catalog Section -->
+    <div class="min-h-screen flex flex-row py-16">
+
+        <!-- Filter -->
+        <div class="w-1/3 bg-black">
+            <h3>Filter</h3>
+        </div>
+
+        <!-- Products -->
+        <div class="w-2/3 flex flex-col items-center px-6">
+
+            <!-- Page Information -->
+            <div class="w-full flex flex-row justify-between font-josefinsans pb-2">
+                <h3>Showing 1 - 16 of 35 results</h3>
+
+                <h3>Sort By: Price High to Low</h3>
+            </div>
+
+            <!-- Product Cards -->
+            <div class="w-full grid grid-cols-4 gap-y-6">
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+                <x-product-card />
+            </div>
+        </div>
+    </div>
+
+    <!-- Product Page Buttons -->
+    <div class="flex flex-row w-full justify-center mb-16 gap-2">
+        <!-- Number Button (Active Tab) -->
+        <div class="w-8 aspect-square rounded-full bg-secondary flex flex-row justify-center items-center group cursor-pointer">
+            <h3 class="text-white">1</h3>
+        </div>
+
+        <!-- Number Button -->
+        <div class="w-8 aspect-square rounded-full bg-primaryDark flex flex-row justify-center items-center group hover:bg-secondary cursor-pointer">
+            <h3 class="text-black group-hover:text-white">2</h3>
+        </div>
+
+        <!-- Next Button -->
+        <div class="w-8 aspect-square rounded-full bg-primaryDark flex flex-row justify-center items-center group hover:bg-secondary cursor-pointer">
+            <img src="<?= asset('icon/Expand_right.svg') ?>" />
+        </div>
+    </div>
+
+</div>
 @endsection
