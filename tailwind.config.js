8d0194b36425d97959a949ad5f3765dd68da31d1
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2023-10-22 17:59:10 +0700

    (Add) bg image and colors in Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ export default {
     ],
     theme: {
         extend: {
+            backgroundImage: {
+                hero: "url('/public/image/hero.png')",
+            },
             colors: {
                 bg: "rgba(var(--color-bg), <alpha-value>)",
+                primaryDark: "#EAE5D5",
                 secondary: "rgba(var(--color-secondary), <alpha-value>)",
                 accent: "rgba(var(--color-accent), <alpha-value>)",
             },
@@ -21,19 +25,19 @@ export default {
     plugins: [require("daisyui")],
     daisyui: {
         themes: [
-          {
-            customTheme: {
-              "primary": "#F5F3E8",
-              "secondary": "#005B49",
-              "accent": "#F78764",  
-              "neutral": "#F7F7F7",
-              "base-100": "#EAE5D5",
-              "info": "#",
-              "success": "#06A385",
-              "warning": "#",
-              "error": "#E5383B",
+            {
+                customTheme: {
+                    primary: "#F5F3E8",
+                    secondary: "#005B49",
+                    accent: "#F78764",
+                    neutral: "#F7F7F7",
+                    "base-100": "#EAE5D5",
+                    info: "#",
+                    success: "#06A385",
+                    warning: "#",
+                    error: "#E5383B",
+                },
             },
-          },
         ],
-      },
+    },
 };
