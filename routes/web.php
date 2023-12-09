9f58cf263902dbe40786b58e7ca46f883ab97441
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2023-12-09 21:21:03 +0700

    (Add) Move redirection to new AdminController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -53,30 +54,36 @@ Route::get('/checkout', function () {
     return view('screens/checkout');
 });
 
-Route::get('/admin', function () {
-    return view('screens/admin');
-});
+// Route::get('/admin', function () {
+//     return view('screens/admin');
+// });
 
-Route::get('/admin/dashboard', function () {
-    return view('screens/dashboard');
-});
+// Route::get('/admin/dashboard', function () {
+//     return view('screens/dashboard');
+// });
 
-Route::get('/admin/product', function () {
-    return view('screens/product');
-});
+// Route::get('/admin/product', function () {
+//     return view('screens/product');
+// });
 
-Route::get('/admin/transaction', function () {
-    return view('screens/transaction');
-});
+// Route::get('/admin/transaction', function () {
+//     return view('screens/transaction');
+// });
 
-Route::get('/admin/transaction-detail', function () {
-    return view('screens/transaction-detail');
-});
+// Route::get('/admin/transaction-detail', function () {
+//     return view('screens/transaction-detail');
+// });
 
-Route::get('/admin/new-product', function () {
-    return view('screens/newproduct');
-});
+Route::get('/admin', [AdminController::class, 'loginView'])->name('admin.loginView');
 
-Route::get('/admin/edit-product', function () {
-    return view('screens/editproduct');
-});
+Route::get('/admin/dashboard', [AdminController::class, 'home'])->name('admin.home');
+
+Route::get('/admin/product', [AdminController::class, 'products'])->name('admin.products');
+
+Route::get('/admin/transaction', [AdminController::class, 'transactions'])->name('admin.transactions');
+
+Route::get('/admin/transaction-detail', [AdminController::class, 'transactionDetails'])->name('admin.transactionDetails');
+
+Route::get('/admin/new-product', [AdminController::class, 'newProduct'])->name('admin.newProduct');
+
+Route::get('/admin/edit-product', [AdminController::class, 'editProduct'])->name('admin.editProduct');
